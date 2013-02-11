3e9fbb7913bb7749b51d7a0eed1c230a01773300
Jonas Baumann <jone@jone.ch>
2013-02-11 14:30:59 +0100

    Bootstrap package

---
+++ README.rst
@@ -0,0 +1,34 @@
+onegov.policy
+=============
+
+`onegov.policy`, the policy package for `OneGov Box`_ pulls together and configures the
+OneGov modules.
+
+
+.. contents:: Table of Contents
+
+
+Installing OneGov
+-----------------
+
+For details on how to install `OneGov Box`_ see the instructions in the readme of the
+`onegov-buildout <https://github.com/OneGov/onegov-buildout/>`_.
+
+
+Links
+-----
+
+- Buildout: https://github.com/OneGov/onegov-buildout
+- Source: https://github.com/OneGov/onegov.policy
+- Issue tracker: https://github.com/OneGov/onegov.policy/issues
+
+
+Copyright
+---------
+
+This package is copyright by `Verein OneGov <http://www.onegov.ch/>`_.
+
+``onegov.policy`` is licensed under GNU General Public License, version 2.
+
+
+.. _OneGov Box: http://www.onegov.ch/
---
+++ setup.py
@@ -0,0 +1,103 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.dev0'
+
+
+tests_require = ['plone.app.testing',
+                 'plone.mocktestcase',
+                 'ftw.testing',
+                 ]
+
+
+extras_require = {
+    'tests': tests_require,
+    }
+
+
+setup(name='onegov.policy',
+      version=version,
+      description="OneGov Box policy package",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        ],
+
+      keywords='onegov box policy',
+      author='Verein OneGov',
+      url='https://github.com/OneGov/onegov.policy',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['onegov'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+
+        'ftw.billboard',
+        'ftw.blog',
+        'ftw.book',
+        'ftw.bridge.client',
+        'ftw.calendarwidget',
+        'ftw.colorbox',
+        'ftw.contentmenu',
+        'ftw.dashboard.dragndrop',
+        'ftw.dashboard.portlets.favourites',
+        'ftw.dashboard.portlets.postit',
+        'ftw.dashboard.portlets.recentlymodified',
+        'ftw.dictstorage',
+        'ftw.file',
+        'ftw.geo',
+        'ftw.globalstatusmessage',
+        'ftw.inflator',
+        'ftw.journal',
+        'ftw.keywordoverlay',
+        'ftw.meeting',
+        'ftw.notification.email',
+        'ftw.participation',
+        'ftw.pdfgenerator',
+        'ftw.permissionmanager',
+        'ftw.poodle',
+        'ftw.portlet.clock',
+        'ftw.portlet.weather',
+        'ftw.sendmail',
+        'ftw.tabbedview',
+        'ftw.table',
+        'ftw.tagging',
+        'ftw.task',
+        'ftw.tooltip',
+        'ftw.upgrade',
+        'ftw.usermanagement',
+        'ftw.workspace',
+        'ftwbook.graphicblock',
+
+        'izug.ticketbox',
+
+        'seantis.dir.base',
+
+        'simplelayout.base',
+        'simplelayout.types.common',
+        'simplelayout.types.flowplayerblock',
+        'simplelayout.types.news',
+        'simplelayout.ui.base',
+        'simplelayout.ui.dragndrop',
+        ],
+
+      tests_require=tests_require,
+      extras_require=extras_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
